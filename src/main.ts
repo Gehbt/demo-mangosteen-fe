ec53022cb2a80b6e4ad3e1084501ba11344a2a7a
Gehbt <wos54lol@gmail.com>
2023-10-08 13:26:16 +0800

    refac: iso history, router with main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import { App } from "./App";
+import { createRouter } from "vue-router";
+import { routes } from "@/routes";
+import { history } from "@/shared/history";
+// const history = createWebHashHistory();
+// 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+const router = createRouter({history,routes,});
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Bar } from "@/components/Bar";
+import { Foo } from "@/components/Foo";
+
+export const routes = [
+    { path: "/", component: Foo },
+    { path: "/about", component: Bar },
+  ];
