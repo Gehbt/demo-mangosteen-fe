dceefcdd37a523f038421b2e0a6e43d445c61b6a
Gehbt <wos54lol@gmail.com>
2023-11-16 19:29:31 +0800

    feat(start): add Center

---
+++ src/components/Center.tsx
@@ -0,0 +1,19 @@
+import { PropType, defineComponent } from "vue";
+import s from "./Center.module.scss";
+
+export const Center = defineComponent({
+  props: {
+    direction: {
+      type: String as PropType<"horizontal" | "vertical">,
+      required: false,
+      default: "horizontal",
+    },
+  },
+  setup(props, context) {
+    const direction =
+      props.direction === "horizontal" ? s.horizontal : s.vertical;
+    return () => (
+      <div class={[s.center, direction]}>{context.slots.default?.()}</div>
+    );
+  },
+});
--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -1,6 +1,9 @@
 import { Button, Floatbutton } from "@/components/Button";
 import { defineComponent } from "vue";
 import s from "./Start.module.scss";
+import { Center } from "@/components/Center";
+import svg from "@svg_map";
+import SvgIcon from "@/components/SvgIcon";
 const onClick = () => {
   console.log(" :>> ");
 };
@@ -8,6 +11,10 @@ export const Start = defineComponent({
   setup() {
     return () => (
       <div>
+        <nav>menu</nav>
+        <Center direction="vertical" class={s.svg_wrapper}>
+          <SvgIcon name={svg.piggy2} class={s.svg} />
+        </Center>
         <div class={s.btn_wrapper}>
           <Button class={s.btn} onClick={onClick}>
             测试
