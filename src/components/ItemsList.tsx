35de8c0d37edb43bd74286f3b828f5fc756a3e94
Gehbt <wos54lol@gmail.com>
2023-11-27 16:42:49 +0800

    feat(items-list): add item-summery

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref, watchEffect } from "vue";
 // import s from "./ItemsList.module.scss";
 import svg from "@svg_map";
 import { MainLayout } from "@/layouts/MainLayout";
@@ -6,11 +6,41 @@ import { Tab, Tabs } from "./Tabs";
 import { InputPad } from "./InputPad";
 import s from "./ItemsList.module.scss";
 import SvgIcon from "./SvgIcon";
+import { ItemSummary } from "./ItemSummary";
+import { Time } from "@/composables/date";
+import { Overlay } from "vant";
+export type ItemsListName = "本月" | "上月" | "今年" | "自定义";
 export const ItemsList = defineComponent({
   name: "ItemsList",
   setup(props, context) {
     const router = useRouter();
-    const refSelected = ref<"本月" | "上月" | "今年" | "自定义">("本月");
+    const refSelected = ref<ItemsListName>("本月");
+    const standardStartDate = "2023-01-01";
+    const time = new Time();
+    const customTime = reactive({
+      start: new Time(),
+      end: new Time(),
+    });
+    const timeList = [
+      {
+        start: time.firstDayOfMonth(),
+        end: time.lastDayOfMonth(),
+      },
+      {
+        start: time.add(-1, "month").firstDayOfMonth(),
+        end: time.add(-1, "month").lastDayOfMonth(),
+      },
+      {
+        start: time.firstDayOfYear(),
+        end: time.lastDayOfYear(),
+      },
+    ];
+    const refOverlayVisible = ref(false);
+    watchEffect(() => {
+      if (refSelected.value === "自定义") {
+        refOverlayVisible.value = true;
+      }
+    });
     return () => (
       <MainLayout
         title="蓝莓记账"
@@ -23,36 +53,46 @@ export const ItemsList = defineComponent({
       >
         {{
           default: () => (
-            <div class={s.itemsList}>
-              <div class={s.tabs}>
-                <Tabs
-                  v-model:selected={refSelected.value}
-                  class={s.tabs}
-                  classPrefix={"customTabStyle"}
-                >
-                  <Tab name="本月">
-                    <div class={s.tab}>
-                      <span>1</span>
-                    </div>
-                  </Tab>
-                  <Tab name="上月">
-                    <div class={s.tab}>
-                      <span>2</span>
-                    </div>
-                  </Tab>
-                  <Tab name="今年">
-                    <div class={s.tab}>
-                      <span>3</span>
-                    </div>
-                  </Tab>
-                  <Tab name="自定义">
-                    <div class={s.tab}>
-                      <span>4</span>
-                    </div>
-                  </Tab>
-                </Tabs>
-              </div>
-            </div>
+            <>
+              <Tabs
+                v-model:selected={refSelected.value}
+                class={s.tabs}
+                classPrefix={"customTabStyle"}
+              >
+                <Tab name="本月">
+                  <ItemSummary
+                    startDate={timeList[0].start.format()}
+                    endDate={timeList[0].end.format()}
+                  />
+                </Tab>
+                <Tab name="上月">
+                  <ItemSummary
+                    startDate={timeList[1].start.format()}
+                    endDate={timeList[1].end.format()}
+                  />
+                </Tab>
+                <Tab name="今年">
+                  <ItemSummary
+                    startDate={timeList[2].start.format()}
+                    endDate={timeList[2].end.format()}
+                  />
+                </Tab>
+                <Tab name="自定义">
+                  <ItemSummary startDate={standardStartDate} endDate={"123"} />
+                </Tab>
+              </Tabs>
+              <Overlay show={refOverlayVisible.value} class={s.overlay}>
+                <div class={s.overlay_inner}>
+                  <header>请选择时间</header>
+                  <main>
+                    <form>
+                      <div>开始时间</div>
+                      <div>结束时间</div>
+                    </form>
+                  </main>
+                </div>
+              </Overlay>
+            </>
           ),
         }}
       </MainLayout>
@@ -60,7 +100,7 @@ export const ItemsList = defineComponent({
   },
 });
 
-export type ItemsName = "支出" | "收入";
+export type ItemsCreateName = "支出" | "收入";
 export const ItemsCreate = defineComponent({
   name: "ItemsCreate",
   setup(props, context) {
