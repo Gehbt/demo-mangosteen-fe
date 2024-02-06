c11e4764578efedcfa7836c8b1fd9da94b6caeec
Gehbt <wos54lol@gmail.com>
2024-02-06 20:03:03 +0800

    chore: export-index with format

--- src/scripts/gen-export-index.ts
+++ src/scripts/gen-export-index.ts
@@ -6,24 +6,25 @@ const command = "ls -d ./src/*/";
 // ./src/assets  ./src/components   ./src/layouts  ./src/scripts  ./src/views
 // ./src/batch   ./src/composables  ./src/routes   ./src/shared   ./src/vite_plugins
 const doDir = ["composables", "shared", "mock"];
-let exludeFile = ["index", "type"];
 doDir.map((dir) => {
+  let exludeFile = ["index", "type"];
   const dir_list = readdirSync("./src/" + dir).map((p) => basename(p, ".ts"));
   const asm = dir_list.map((file) => {
     // 不为一些文件生成导出
     if (exludeFile.includes(file)) {
       // just for fun
       exludeFile = exludeFile.filter((ef) => ef !== file);
+      console.log("exludeFile :>> ", exludeFile);
       return;
     }
-    return `export * from './${file}'\n`;
+    return `export * from "./${file}";\n`;
   });
 
   writeFile(`./src/${dir}/index.ts`, asm.join(""), (err) => {
     if (err) throw err;
   });
 });
-console.log(`generated for \`${doDir.join()}\`
+console.log(`generated export index for \`${doDir.join()}\`
 # ---------------------------------------------------------------------------- #
 #                        The export index generated!                           #
 # ---------------------------------------------------------------------------- #`);
